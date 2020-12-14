61d8a6fa27a6ecb99c94558ab86d00dd82061fd2
aliabdelhai <72930982+aliabdelhai@users.noreply.github.com>
2020-12-14 18:17:12 +0200

    add Stack

--- Stacks&Queues/src/Stack.js
+++ Stacks&Queues/src/Stack.js
@@ -1,22 +1,18 @@
 
-class Queue{
+class Stack{
     constructor(){
-        this.queue = []
+        this.stack = []
         this.length = 0;
     }
 
-    enqueue(x){
-        this.queue.unshift(x)
-        this.length++
+    push(x){
+        this.stack[this.length] = x;
+        this.length++;
 
     }
 
-    dequeue(){
-        if(this.isEmpty()){
-            return null;
-        }
-        this.length--;
-        return delete this.queue[this.length]
+    isEmpty(){
+        return this.length == 0;
 
     }
 
@@ -24,22 +20,24 @@ class Queue{
         if(this.isEmpty()){
             return null;
         }
-        return this.queue[this.length-1]
-
+        return this.stack[this.length - 1];
     }
 
-    isEmpty(){
-        return this.length == 0;
+    pop(){
+        if(this.isEmpty()){
+            return null;
+        }
+        this.length--;
+        return delete this.stack[this.length]
     }
 
     print(){
-        console.log(this.queue)
+        console.log(this.stack)
     }
-
-
 }
 
 
+
 /* Do not remove the exports below */
-module.exports = Queue
+module.exports = Stack
 
