0d84d3333b921cc39e601680a0c241935c106aaa
aliabdelhai <72930982+aliabdelhai@users.noreply.github.com>
2020-12-20 16:08:11 +0200

    components

---
+++ react-components-exercise/scr/components/Checkout.js
@@ -0,0 +1,15 @@
+import Item from "./Item";
+import React, { Component } from 'react';
+
+
+class Checkout extends Component {
+    render() {
+        return (
+            <div>
+                <div> Checkout Component </div>
+                <Item />
+            </div>    
+        )
+    }
+}
+export default Checkout
