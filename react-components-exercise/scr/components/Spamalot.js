5b51302f5ffae201efd2b2dc70a54bdd350b1073
aliabdelhai <72930982+aliabdelhai@users.noreply.github.com>
2020-12-20 16:10:41 +0200

    components

---
+++ react-components-exercise/scr/components/Spamalot.js
@@ -0,0 +1,20 @@
+import Spam from "./Spam";
+import React, { Component } from 'react';
+
+
+class Spamalot extends Component {
+    render() {
+        let spams = []
+        for(let i =0; i<500; i++)
+        {
+            spams.push(<Spam />)
+        }
+
+        return (
+            spams
+        )
+    }
+}
+
+export default Spamalot
+
